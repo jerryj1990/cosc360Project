8790e4ef6225603716fa5c7860818a8df0c8c518
Kai Chieh Hsu <jerryjerry1990@hotmail.com>
2013-11-17 08:35:01 +0800

    reformat form and login

--- bootstrap/login.php
+++ bootstrap/login.php
@@ -4,8 +4,10 @@
     <meta charsest="utf-8"/>
     <title>Login</title>
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	 <link rel="stylesheet" type="text/css" href="style.css">
         <script type="text/javascript" src="http://code.jquery.com/jquery.min.js"></script>
         <script type="text/javascript" src="js/bootstrap.min.js"></script>
+		
     <style type="text/css">
             #login{
                 margin: 15% auto;
@@ -14,7 +16,12 @@
         </style>
 </head>
 <body>
+	<header>
+	 <img src="UBC.png"></a>          
+	</header>
     <?php include 'functions.php'; ?>
+	<div id = "wrapper">
+				<div id = "content">
 	<?php
 						if (isset$_Session['ID']) {
 							echo 'You are already logged in!';
@@ -23,7 +30,7 @@
 							if (!empty($_GET)) {
 								echo 'Login Failed. Please try again.';
 							}
-							echo '<form class="well" id="login" action="verify.php" method="post">';
+							echo '<center><form class="well" id="login" action="verify.php" method="post">';
 							echo '<label>Username</label>';
 							echo '<input type="text" class="span3" name="uid"/></br>';
 							echo '<label>Password</label>';
@@ -31,10 +38,11 @@
 							echo '<br><br>';
 							echo '<button class="btn btn-primary">Submit</button>';
 							echo '<button class="btn">Clear</button>';
-							echo '</form>';
+							echo '</form></center>';
 						}
 	?>
-
+				</div>
+	</div>
     <script src="js/bootsrap.js"></script>
 </body>
 </html>
