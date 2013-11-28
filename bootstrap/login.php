c6b4ad274deb4a5346cae4b16271a24f23e86cec
Kai Chieh Hsu <jerryjerry1990@hotmail.com>
2013-11-28 11:53:47 +0800

    picture taken at login page

--- bootstrap/login.php
+++ bootstrap/login.php
@@ -37,12 +37,58 @@
 							echo '<input type="text" class="span3" name="uid"/></br>';
 							echo '<label>Password</label>';
 							echo '<input type="password" class="span3" name="upass"/></br>';
-							echo '<br><br>';
-							echo '<button class="btn btn-primary">Submit</button>';
+							echo '<video id="video" width="200" height="160"></video>';
+							echo '<button id="button" class="btn btn-primary">Submit</button>';
 							echo '<button class="btn">Clear</button>';
 							echo '</form></center>';
+							echo '<div id="canvasHolder"></div>';
 						}
 	?>
+
+	    <script>
+
+        var video;
+        var dataURL;
+
+        function setup() {
+            navigator.myGetMedia = (navigator.getUserMedia ||
+            navigator.webkitGetUserMedia ||
+            navigator.mozGetUserMedia ||
+            navigator.msGetUserMedia);
+            navigator.myGetMedia({ video: true }, connect, error);
+        }
+
+        function connect(stream) {
+            video = document.getElementById("video");
+            video.src = window.URL ? window.URL.createObjectURL(stream) : stream;
+            video.play();
+        }
+
+        function error(e) { console.log(e); }
+
+        addEventListener("load", setup);
+
+        function captureImage() {
+            var canvas = document.createElement('canvas');
+            canvas.id = 'hiddenCanvas';
+            //add canvas to the body element
+            document.body.appendChild(canvas);
+            //add canvas to #canvasHolder
+            document.getElementById('canvasHolder').appendChild(canvas);
+            var ctx = canvas.getContext('2d');
+            canvas.width = video.videoWidth / 4;
+            canvas.height = video.videoHeight / 4;
+            ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+            //save canvas image as data url
+            window.open(canvas.toDataURL('image/png'));
+			
+        }
+
+        //Bind a click to a button to capture an image from the video stream
+        var el = document.getElementById("button");
+        el.addEventListener("click", captureImage, false);
+
+    </script>
 				</div>
 	</div>
     <script src="js/bootsrap.js"></script>
