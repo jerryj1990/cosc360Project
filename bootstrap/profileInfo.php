84f29d384acd44ecf3af44bd7df7840e19352819
pgunawan <pamela.gunawan@yahoo.com>
2013-11-28 17:01:08 -0800

    drag&drop picture

--- bootstrap/profileInfo.php
+++ bootstrap/profileInfo.php
@@ -1,3 +1,12 @@
+<style>
+#holder { border: 4px solid #ccc; width:200px; min-height: 200px; margin: 20px auto;}
+#holder.hover { border: 4px dashed #0c0; }
+#holder img { display: block; margin: 5px auto; }
+#holder p { margin: 0px; font-size: 14px; }
+.fail { background: #c00; padding: 2px; color: #fff; }
+    .hidden { display: none !important;}
+    </style>
+    
 <?php
 ini_set('display_errors', 'On');
 error_reporting(E_ALL);
@@ -20,6 +29,108 @@ error_reporting(E_ALL);
     $result->execute();
     $result = $result->fetch();
 ?>
+    
+<!--Drag & Drop Picture-->
+    <div id="holder"></div>
+    <p id="upload" class="hidden">
+    <label>Drag & drop not supported<br><input type="file"></label></p>
+    <p id="filereader">File API & FileReader API not supported</p>
+    <p id="formdata">XHR2's FormData is not supported</p>
+    <p id="progress">XHR2's upload progress isn't supported</p>
+    
+    <script>
+    var holder = document.getElementById('holder'),
+    tests = {
+    filereader: typeof FileReader != 'undefined',
+    dnd: 'draggable' in document.createElement('span'),
+    formdata: !!window.FormData,
+    progress: "upload" in new XMLHttpRequest
+    },
+    support = {
+    filereader: document.getElementById('filereader'),
+    formdata: document.getElementById('formdata'),
+    progress: document.getElementById('progress')
+    },
+    acceptedTypes = {
+        'image/png': true,
+        'image/jpeg': true,
+        'image/gif': true
+    },
+    progress = document.getElementById('uploadprogress'),
+    fileupload = document.getElementById('upload');
+    
+    "filereader formdata progress".split(' ').forEach(function (api) {
+                                                      if (tests[api] === false) {
+                                                      support[api].className = 'fail';
+                                                      } else {
+                                                      support[api].className = 'hidden';
+                                                      }
+                                                      });
+    
+    function previewfile(file) {
+        if (tests.filereader === true && acceptedTypes[file.type] === true) {
+            var reader = new FileReader();
+            reader.onload = function (event) {
+                var image = new Image();
+                image.src = event.target.result;
+                image.width = 200; // a fake resize
+                image.height = 200;
+                holder.appendChild(image);
+            };
+            
+            reader.readAsDataURL(file);
+        }  else {
+            holder.innerHTML += '<p>Uploaded ' + file.name + ' ' + (file.size ? (file.size/1024|0) + 'K' : '');
+            console.log(file);
+        }
+    }
+    
+    function readfiles(files) {
+        debugger;
+        var formData = tests.formdata ? new FormData() : null;
+        for (var i = 0; i < files.length; i++) {
+            if (tests.formdata) formData.append('file', files[i]);
+            previewfile(files[i]);
+        }
+        
+        //post a new XHR request
+        if (tests.formdata) {
+            var xhr = new XMLHttpRequest();
+            xhr.open('POST', '/devnull.php');
+            xhr.onload = function() {
+                progress.value = progress.innerHTML = 100;
+            };
+            
+            if (tests.progress) {
+                xhr.upload.onprogress = function (event) {
+                    if (event.lengthComputable) {
+                        var complete = (event.loaded / event.total * 100 | 0);
+                        progress.value = progress.innerHTML = complete;
+                    }
+                }
+            }
+            
+            xhr.send(formData);
+        }
+    }
+    
+    if (tests.dnd) {
+        holder.ondragover = function () { this.className = 'hover'; return false; };
+        holder.ondragend = function () { this.className = ''; return false; };
+        holder.ondrop = function (e) {
+            this.className = '';
+            e.preventDefault();
+            readfiles(e.dataTransfer.files);
+        }
+    } else {
+        fileupload.className = 'hidden';
+        fileupload.querySelector('input').onchange = function () {
+            readfiles(this.files);
+        };
+    }
+    
+    </script>
+
 
 <!-- Profile Information -->
 
